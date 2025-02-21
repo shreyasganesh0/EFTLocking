9b2d3c09c8255c5fcc1a9938ab8c984322cd646b
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-02-21 13:35:14 -0500

    Update types and finish account init code

--- include/main.h
+++ include/main.h
@@ -2,20 +2,33 @@
 #define MAIN_H
 
 
-struct {
-    int acc_no; 
-    int amount;
-} account_t;
+#define READ_BYTES 4096
+#define TXN_BUF_SIZE 10 //should not be needed for this case 
+
+char *file_buf;
+int num_users;
 
 struct {
-    pthread_mutex_t acc1_mtx;
-    pthread_mutex_t acc2_mtx;
-} worker_t;
+    pthread_mutex_t acc_mtx;
+    int acc_no;
+    int balance;
+} account_t;
+account_t* accounts;
 
 struct {
     account_t sender;
     account_t reciver;
+    int trans_amt;
 } txn_t;
+
+struct {
+    txn_t txn_buf[TXN_BUF_SIZE];
+    sem_t full;
+    sem_t empty;
+    int in;
+    int out;
+} worker_t;
+worker_t *workers;
 
 #endif
 
--- src/main.c
+++ src/main.c
@@ -10,18 +10,14 @@
 #include "main.h"
 #include "syscalls.c"
 
-#define READ_BYTES 4096
-
-char *file_buf;
-int num_users;
-
+int acc_capcity = 200;
 int main(int argc, char *argv[]) {
 
     if (argc < 3) {
         my_write(1, "Invalid arguments\n");
         return -1;
     }
-    num_users = atoi(argv[2]);
+    num_workers = atoi(argv[2]);
     
     int fd = open(argv, O_RDWR, 666);
     if (fd == -1) {
@@ -36,40 +32,121 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     
-    worker_t *workers = malloc(num_users * sizeof(worker_t)); 
-    for (int i = 0; i < num_users; i++) {
-        pthread_mutex_init(&workers[i].acc1_mtx, NULL);
+    workers = malloc(num_workers * sizeof(worker_t));
+    for (int i = 0; i < num_workers; i++) {
         pthread_mutex_init(&workers[i].acc2_mtx, NULL);
     }
     account_setup(fd);
+
+    tid_t distribute_tid;
+    pthread_init(&distribute_tid, distributor, &fd);
+
     
     return 0;
 }
 
+void *distributor(void *arg) {
+
+    int fd = *(int *)arg;
+    ssize_t bytes_read;  
+    char buf[READ_BYTES];
+
+    while((bytes_read = read(fd, buf, READ_BYTES) > 0) {
+        
+        char *ptr = buf;
+        char *end_ptr = ptr + bytes_read;
+        
+        while((char *newline = memchr(ptr, '\n', end_ptr - ptr) != NULL) {
+            // this is the account setup loop
+
+            *newline = '\0';
+            char *space = memchr(ptr, ' ', newline - ptr);
+            *space = '\0';
+            if (strcmp(ptr, "Transfer")) {
+                my_write(1, "Invalid transaction detected\n");
+                ptr = newline;
+                ptr++;
+                continue;
+            }
+            ptr = space;
+            ptr++; //jmp from id
+
+            space = memchr(ptr, ' ', newline - ptr);
+            *space = '\0';
+            int from_idx = atoi(ptr);
+            ptr = space;
+            ptr++; //jmp to_id
+
+            space = memchr(ptr, ' ', newline - ptr);
+            *space = '\0';
+            int to_idx = atoi(ptr);
+            ptr = space;
+            ptr++; //jmp to transfer amount
+
+            int amt = atoi(ptr);
+            
+            idx++;
+
+            ptr = newline;
+            ptr++;//jmp to next line start chr
+        }
+
+        off_t curr_offset = lseek(fd, 0, SEEK_CUR);
+        lseek(fd, curr_offset - (end_ptr - ptr), SEEK_SET); //reset the cursor on exit to avoid skipping bytes
+    }
+}
+
+
 void accounts_setup(int fd) {
     ssize_t bytes_read;  
     char buf[READ_BYTES];
-    temp_acc_arr[200];
+    accounts = malloc(capacity * sizeof(account_t));
     int idx = 0;
 
     while((bytes_read = read(fd, buf, READ_BYTES) > 0) {
         
         char *ptr = buf;
+        char *end_ptr = ptr + bytes_read;
+        char transfer_flag = '\0';
         
         
-        while((char *newline = memchr(ptr, '\n', bytes_read) != NULL) {
+        while((char *newline = memchr(ptr, '\n', end_ptr - ptr) != NULL) {
+            // this is the account setup loop
 
             *newline = '\0';
             ehar *space = memchr(ptr, ' ', newline - ptr);
+
             *space = '\0';
-            account_t account.acc_no = atoi(ptr);
-            ptr = space;
-            ptr++;
-            account.amount = atoi(ptr);
+            if (!strcmp(ptr, "Transfer")) {
+                transfer_flag = 'x';
+                break;
+            }
             
-            temp_acc_arr[idx] = account;
+            accounts[idx].acc_no = idx + 1;
+            ptr = space;
+            ptr++; //jmp to amount
+                   
+            accounts[idx].balance = atoi(ptr);
+             
+            pthread_mutex_init(&accounts[idx].acc_mtx, NULL);
             idx++;
+
+            ptr = newline;
+            ptr++;//jmp to next line start chr
         }
+
+        off_t curr_offset = lseek(fd, 0, SEEK_CUR);
+        lseek(fd, curr_offset - (end_ptr - ptr), SEEK_SET); //reset the cursor on exit to avoid skipping bytes
+
+        if (transfer_flag) {
+            break;
+        }
+    }
+}
+
+
+
+
         
              
              
